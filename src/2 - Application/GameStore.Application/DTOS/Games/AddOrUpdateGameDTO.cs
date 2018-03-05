c470caa1d3958663c7a594291c34d6c12accd977
admin <admin>
2018-03-05 18:48:46 -0300

    add required filds

--- src/2 - Application/GameStore.Application/DTOS/Games/AddOrUpdateGameDTO.cs
+++ src/2 - Application/GameStore.Application/DTOS/Games/AddOrUpdateGameDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using GameStore.Domain.Entities.ReleationshipEntities;
 
 namespace GameStore.Application.DTOS.Games
@@ -7,15 +8,21 @@ namespace GameStore.Application.DTOS.Games
     public class AddOrUpdateGameDTO
     {
         public Guid Id { get; set; }
+        [Required()]
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
+        [Required()]
         public double Score { get; set; }
+        [Required()]
         public string Description { get; set; }
         public string ShortDescription { get; set; }
         public string ImageUrl { get; set; }
 
+        [Required(ErrorMessage = "Developers(s) required.")]
         public ICollection<GameDeveloper> GameDevelopers { get; set; }
+        [Required(ErrorMessage = "Plataform(s) required.")]
         public ICollection<GamePlataform> GamePlataforms { get; set; }
+        [Required(ErrorMessage = "Genre(s) required.")]
         public ICollection<GameGenre> GameGenres { get; set; }
         public ICollection<GamePublisher> GamePublishers { get; set; }
     }
